e283c36ddac36a85832d8f4f122f37d3b75aeb67
soham-pravin-parab <sohamparabmail@gmail.com>
2026-05-10 12:32:30 -0400

    Add files via upload

---
+++ Array/Leetcode 88 Merge sorted array/Leetcode88.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void merge(vector<int>& A, int m, vector<int>& B, int n) {
+        int idx = m+n-1;
+        int i = m-1;
+        int j = n-1;
+        while(i>=0 && j>=0){
+            if(A[i]>B[j]){
+                A[idx]=A[i];
+                idx--;i--;
+            }
+            else{
+                A[idx]=B[j];
+                idx--;j--;
+            }
+        }
+        while(j>=0){
+            A[idx]=B[j];
+            idx--;j--;
+        }
+    }
+};
