cebd772c6c9bc77236b705222ef29571fb1559c1
soham-pravin-parab <sohamparabmail@gmail.com>
2026-07-04 21:17:38 -0400

    Add files via upload

---
+++ Array/Leetcode 560  Subarray Sum Equals to k/Leetcode560.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_map<int,int>m;
+        m[0]=1;
+        int sum =0;
+        int ans = 0 ;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            if(m.find(sum-k)!=m.end()){
+                ans+=m[sum-k];
+            
+            }
+            m[sum]++;
+        }
+        return ans;
+        
+    }
+};
