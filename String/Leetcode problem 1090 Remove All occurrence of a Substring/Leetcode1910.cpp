c651cc5b1d2024a9737697bb5456489e699f4ef5
soham-pravin-parab <sohamparabmail@gmail.com>
2026-07-23 23:05:11 +0530

    Delete String/Leetcode problem 1090 Remove All occurrence of a Substring/Leetcode1910.cpp

--- String/Leetcode problem 1090 Remove All occurrence of a Substring/Leetcode1910.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    string removeOccurrences(string s, string part) {
-        while (s.size() > 0 && s.find(part) < s.size()) {
-            s.erase(s.find(part), part.length());
-        }return s;
-    }
-};
